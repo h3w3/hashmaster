0fb943ab036facc779a682534e017d12939deea2
Burger Queen <h3w3onon@gmail.com>
2025-11-06 22:01:51 -0600

    customize award admin columns

--- stats/admin.py
+++ stats/admin.py
@@ -4,12 +4,13 @@ from .models import StatsYear, StatsYearRoles, Hasher, Role, Award, Trail, Pack
 class StatsYearRolesAdmin(admin.ModelAdmin):
     list_display = ('stats_year_id', 'role_id', 'office_holder')
 
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('num_trails', 'award_name', 'stats_year_id')
+
 admin.site.register(StatsYear)
 admin.site.register(StatsYearRoles, StatsYearRolesAdmin)
 admin.site.register(Hasher)
 admin.site.register(Role)
-admin.site.register(Award)
+admin.site.register(Award, AwardAdmin)
 admin.site.register(Trail)
 admin.site.register(Pack)
-
-# Register your models here.
