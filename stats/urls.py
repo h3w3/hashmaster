e9b95fe68cba7d0b92d0dfaca00eaaa5123604af
Burger Queen <h3w3onon@gmail.com>
2026-01-24 15:40:37 -0600

    expand initial public api including templates

--- stats/urls.py
+++ stats/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     path("trails/<int:trail_id>/trash/", views.trash, name="trash"),
     path("trails/<int:trail_id>/hares/", views.hares, name="hares"),
     path("hashers/", views.hashers, name="hashers"),
+    path("hashers/<int:id>/", views.hasher, name="hasher"),
     path("hashers/<str:hasher_name>/", views.hasher_name, name="hasher_name"),
+    path("years/", views.stats_years, name="stats_years"),
     path("<int:year>/", views.stats_year, name="stats_year"),
+    path("awards/", views.awards, name="awards"),
+    path("awards/<int:num_trails>/", views.award, name="award"),
 
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+from stats.models import Trail
+from stats.models import Hasher
+from stats.models import Award
+from stats.models import StatsYear
 
 def index(request):
-    return HttpResponse("Hello, world. You're looking at the HashMaster stats index.")
+    recent_trails = Trail.objects.order_by("trail_id")[:3]  # most recent 3 trails
+    template = loader.get_template("stats/index.html")
+    context = {"recent_trails": recent_trails}
+    return HttpResponse(template.render(context, request))
 
 def trails(request):
-    return HttpResponse("You're looking at all the trails.")
+    all_trails = Trail.objects.order_by("-trail_id")  # all trails by id
+    template = loader.get_template("stats/trails.html")
+    context = {"all_trails": all_trails}
+    return HttpResponse(template.render(context, request))
 
 def trail(request, trail_id):
-    return HttpResponse("You're looking at stats for trail %s." % trail_id)
+    return HttpResponse("You're looking at stats for trail %i." % trail_id)
 
 def map(request, trail_id):
     return HttpResponse("You're looking at the map for trail %s." % trail_id)
@@ -22,10 +33,31 @@ def hares(request, trail_id):
     return HttpResponse("You're looking at all the hares for trail %s." % trail_id)
 
 def hashers(request):
-    return HttpResponse("You're looking at all the active hashers")
+    all_hashers = Hasher.objects.order_by("hash_name")
+    template = loader.get_template("stats/hashers.html")
+    context = {"all_hashers": all_hashers}
+    return HttpResponse(template.render(context, request))
+
+def hasher(request, id):
+    return HttpResponse("You're looking at the hasher with id %i." % id)
 
 def hasher_name(request, hasher_name):
     return HttpResponse("You're looking at the hasher with name %s." % hasher_name)
 
+def stats_years(request):
+    all_stats_years = StatsYear.objects.order_by("-year")
+    template = loader.get_template("stats/stats_years.html")
+    context = {"all_stats_years": all_stats_years}
+    return HttpResponse(template.render(context, request))
+
 def stats_year(request, year):
     return HttpResponse("You're looking at the annual stats for stats year %s." % year)
+
+def awards(request):
+    all_awards = Award.objects.order_by("num_trails")
+    template = loader.get_template("stats/awards.html")
+    context = {"all_awards": all_awards}
+    return HttpResponse(template.render(context, request))
+
+def award(request, num_trails):
+    return HttpResponse("You're looking at the award winners for %s trails." % num_trails)
