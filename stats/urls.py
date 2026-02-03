684d7d0b5274cd01af65b775ae761a81dd073868
Burger Queen <h3w3onon@gmail.com>
2026-02-03 15:49:16 -0600

    minor consistency changes to url patterns

--- stats/urls.py
+++ stats/urls.py
@@ -10,11 +10,12 @@ urlpatterns = [
     path("trails/<int:trail_id>/pack/", views.pack, name="pack"),
     path("trails/<int:trail_id>/trash/", views.trash, name="trash"),
     path("trails/<int:trail_id>/hares/", views.hares, name="hares"),
+    path("trails/<int:trail_id>/video/", views.video, name="video"),
     path("hashers/", views.hashers, name="hashers"),
     path("hashers/<int:id>/", views.hasher, name="hasher"),
     path("hashers/<str:hasher_name>/", views.hasher_name, name="hasher_name"),
     path("years/", views.stats_years, name="stats_years"),
-    path("<int:year>/", views.stats_year, name="stats_year"),
+    path("years/<int:year>/", views.stats_year, name="stats_year"),
     path("awards/", views.awards, name="awards"),
     path("awards/<int:num_trails>/", views.award, name="award"),
 
