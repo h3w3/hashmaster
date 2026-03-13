3944be3784229d614f53958912c908f683e5ca0c
Burger Queen <h3w3onon@gmail.com>
2026-03-12 21:38:10 -0500

    add help text to admin view

--- stats/models.py
+++ stats/models.py
@@ -30,17 +30,18 @@ class Hasher(models.Model):
     nerd_first = models.CharField(max_length=30)
     nerd_last = models.CharField(max_length=30)
     hash_nickname = models.CharField(max_length=30)
-    just_name = models.CharField(max_length=30)
+    just_name = models.CharField(max_length=30, help_text="Your nerd name and sponsor before you had a hash name")
     GENDER_CHOICES = (
        ('M', 'Male'),
-       ('F', 'Female')
+       ('F', 'Female'),
+       ('P', 'Pet')
      )
     gender = models.CharField(
          max_length=1, choices=GENDER_CHOICES, blank=True,
          null=True, verbose_name="Gender"
     )
-    home_kennel = models.CharField(max_length=30)
-    current_kennel = models.CharField(max_length=30)
+    home_kennel = models.CharField(max_length=30, help_text="First place you hashed")
+    current_kennel = models.CharField(max_length=30, help_text="Kennel you claim now")
     birth_date = models.DateField(blank=True, null=True)
     email = models.EmailField(blank=True, null=False)
     phone = models.CharField(max_length=30, blank=True, null=False)
@@ -53,7 +54,8 @@ class Hasher(models.Model):
     status = models.ForeignKey(HasherStatus, on_delete=models.SET_NULL, null=True)
     hash_cash_balance = models.FloatField()
     hash_cash_exempt = models.BooleanField(blank=True, null=False, default=False)
-    pi_visible= models.BooleanField(blank=True, null=False, default=False)  # personal info visible
+    pi_visible= models.BooleanField(blank=True, null=False, default=False,
+          help_text="Check yes if personal identifiable information is visible to other logged-in hashers")
     def __str__(self):
         return self.hash_name
 
@@ -92,7 +94,7 @@ class Trail(models.Model):
     trail_zip_postal_code = models.CharField(max_length=10, blank=True, null=False)
     trail_start = models.TimeField(blank=True, null=True)
     trail_start_location = models.CharField(max_length=255, blank=True, null=False)
-    checks = models.TextField(blank=True, null=False)
+    checks = models.TextField(blank=True, null=False, help_text="Verbal description of any beer checks")
     trail_end = models.TimeField(blank=True, null=True)
     trail_end_location = models.CharField(max_length=255, blank=True, null=False)
     map = models.FileField(upload_to='trails/map', blank=True, null=False)
@@ -100,27 +102,30 @@ class Trail(models.Model):
     video = models.FileField(upload_to='trails/video', blank=True, null=False)
     distance_turkey = models.FloatField(blank=True, null=False, default=0)
     distance_eagle = models.FloatField(blank=True, null=False, default=0)
-    distance_comment = models.TextField(max_length=255, blank=True, null=False) # Overview from Hash Statistician
+    distance_comment = models.TextField(max_length=255, blank=True, null=False,
+                                        help_text="Trail overview from Hash Statistician")
     temperature = models.FloatField(blank=True, null=True)
-    namings = models.TextField(blank=True, null=False)
-    golden_ratio = models.FloatField(blank=True, null=True)
-    published_in_stats = models.BooleanField()  # check yes when trail stats final for public use
+    namings = models.TextField(blank=True, null=False, help_text="Description of any namings")
+    golden_ratio = models.FloatField(blank=True, null=True, help_text="Units per hasher")
+    published_in_stats = models.BooleanField(help_text="check yes when trail stats final for public view")
     def __str__(self):
         return str(self.trail_id)
 
 class Pack(models.Model):
     trail_id = models.ForeignKey(Trail, on_delete=models.CASCADE)
     hasher_id = models.ForeignKey(Hasher, on_delete=models.CASCADE)
-    name_at_trail = models.CharField(max_length=255) # accounts for name changes over time
-    hare = models.BooleanField(default=False)        # check yes if hare
-    wanker = models.BooleanField(default=False)      # check yes if auto wanker
-    frb = models.BooleanField(default=False)         # check yes if front running dude
-    fbi = models.BooleanField(default=False)         # check yes if front running babe
-    dfl = models.BooleanField(default=False)         # check yes if dead fricking last
-    visitor = models.BooleanField(default=False)     # check yes if visitor
-    transplant = models.BooleanField(default=False)  # check yes if transplant
-    new_boot = models.BooleanField(default=False)    # check yes if new boot
-    dnf = models.BooleanField(default=False)         # check yes if did not finish
+    # accounts for name changes over time
+    name_at_trail = models.CharField(max_length=255, help_text="Named hasher or not, what was their name this trail?")
+    # select hasher pack attributes that apply
+    hare = models.BooleanField(default=False, help_text="check yes if hare")
+    wanker = models.BooleanField(default=False, help_text="check yes if auto wanker")
+    frb = models.BooleanField(default=False, help_text="check yes if front running bastard")
+    fbi = models.BooleanField(default=False, help_text="check yes if first babe in")
+    dfl = models.BooleanField(default=False, help_text="check yes if dead fricking last")
+    visitor = models.BooleanField(default=False, help_text="check yes if visitor")
+    transplant = models.BooleanField(default=False, help_text="check yes if transplant (once ever in a kennel)")
+    new_boot = models.BooleanField(default=False, help_text="check yes if new boot (once ever in a kennel)")
+    dnf = models.BooleanField(default=False, help_text="check yes if did not finish (weak!)")
     class Meta:
         constraints = [
             models.UniqueConstraint(
